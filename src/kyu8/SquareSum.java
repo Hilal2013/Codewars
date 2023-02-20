77c9f0632eac8b564b115e303a0449e92e8f5d5c
Hilal2013 <hilalozkul@gmail.com>
2023-02-19 20:51:51 -0500

    added Remove chars task

---
+++ src/kyu8/SquareSum.java
@@ -0,0 +1,12 @@
+package kyu8;
+
+public class SquareSum {
+    public static int squareSum(int[] n)
+    {
+        int result=0;
+        for (int i = 0; i < n.length; i++) {
+            result+= Math.pow(n[i],2);
+        }
+        return result;
+    }
+}
